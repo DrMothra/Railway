1a9380a12b5da3b25b98659d3f4a6a5fdf967ca1
Tony Glover <tony.glover@nottingham.ac.uk>
2016-02-23 00:26:57 +0000

    Added track and train sprite

--- js/railViz.js
+++ js/railViz.js
@@ -24,11 +24,29 @@ RailApp.prototype.update = function() {
 RailApp.prototype.createScene = function() {
     BaseApp.prototype.createScene.call(this);
 
-    var box = new THREE.BoxGeometry(5, 5, 5);
-    var boxMat = new THREE.MeshLambertMaterial( {color: 0xff0000});
-    var boxMesh = new THREE.Mesh(box, boxMat);
+    var halfWidth = 100, depth = 100;
+    var points = [];
+    points.push(new THREE.Vector3(0, 0, 0));
+    points.push(new THREE.Vector3(halfWidth, 0, 0));
+    points.push(new THREE.Vector3(halfWidth, 0, -depth));
+    points.push(new THREE.Vector3(-halfWidth, 0, -depth));
+    points.push(new THREE.Vector3(-halfWidth, 0, 0));
+    points.push(new THREE.Vector3(0, 0, 0));
 
-    this.scene.add(boxMesh);
+    var spline = new THREE.CatmullRomCurve3(points);
+    var mat = new THREE.LineBasicMaterial( {color: 0x303030});
+    var geom = new THREE.Geometry();
+    geom.vertices = spline.getPoints(50);
+    var splineObject = new THREE.Line(geom, mat);
+    this.scene.add(splineObject);
+
+    var textureLoader = new THREE.TextureLoader();
+    var trainTex = textureLoader.load("images/engine.png");
+    var trainMat = new THREE.SpriteMaterial( {map: trainTex});
+    var engineSprite = new THREE.Sprite(trainMat);
+    engineSprite.scale.set(10, 10, 1);
+    engineSprite.position.y = 5;
+    this.scene.add(engineSprite);
 };
 
 RailApp.prototype.createGUI = function() {
