9e5facb19045bb1eb95461a82e6ce109e715132e
Tony Glover <tony.glover@nottingham.ac.uk>
2016-02-25 00:30:31 +0000

    Animated engine around elipse

--- js/railViz.js
+++ js/railViz.js
@@ -13,11 +13,26 @@ RailApp.prototype = new BaseApp();
 
 RailApp.prototype.init = function(container) {
     BaseApp.prototype.init.call(this, container);
+
+    this.animating = true;
 };
 
 RailApp.prototype.update = function() {
-    var clicked = this.mouseDown;
+    var delta = this.clock.getDelta();
 
+    if(this.animating) {
+        var xPos = this.engineSprite.position.x;
+        var zPos = this.engineSprite.position.z;
+        delta = zPos > 0 ? delta : -delta;
+        this.engineSprite.position.x += (delta * this.movementPerSecond);
+        if(this.engineSprite.position.x > this.engineX) {
+            this.engineSprite.position.x = this.engineX;
+        }
+        this.engineSprite.position.z = Math.sqrt(this.b2 * (1 - ((this.engineSprite.position.x * this.engineSprite.position.x)/this.a2)));
+        if(delta < 0) {
+            this.engineSprite.position.z *= -1;
+        }
+    }
     BaseApp.prototype.update.call(this);
 };
 
@@ -33,20 +48,27 @@ RailApp.prototype.createScene = function() {
     points.push(new THREE.Vector3(-halfWidth, 0, 0));
     points.push(new THREE.Vector3(0, 0, 0));
 
-    var spline = new THREE.CatmullRomCurve3(points);
-    var mat = new THREE.LineBasicMaterial( {color: 0x303030});
-    var geom = new THREE.Geometry();
-    geom.vertices = spline.getPoints(50);
-    var splineObject = new THREE.Line(geom, mat);
-    this.scene.add(splineObject);
+    var innerRad = 225, outerRad = 240, segments = 30;
+    var ringGeom = new THREE.RingGeometry(innerRad, outerRad, segments);
+    var ringMat = new THREE.MeshBasicMaterial( {color: 0x373737} );
+    var ringMesh = new THREE.Mesh(ringGeom,  ringMat);
+    var yScaleFactor = 0.55;
+    ringMesh.scale.y = yScaleFactor;
+    ringMesh.rotation.x = -Math.PI/2;
+    this.scene.add(ringMesh);
 
     var textureLoader = new THREE.TextureLoader();
     var trainTex = textureLoader.load("images/engine.png");
     var trainMat = new THREE.SpriteMaterial( {map: trainTex});
-    var engineSprite = new THREE.Sprite(trainMat);
-    engineSprite.scale.set(10, 10, 1);
-    engineSprite.position.y = 5;
-    this.scene.add(engineSprite);
+    this.engineSprite = new THREE.Sprite(trainMat);
+    this.engineSprite.scale.set(10, 10, 1);
+    this.engineSprite.position.set(0, 5, (outerRad * yScaleFactor) - 7.5);
+    this.engineX = 230;
+    this.engineZ = 124.5;
+    this.a2 = this.engineX * this.engineX;
+    this.b2 = this.engineZ * this.engineZ;
+    this.movementPerSecond = 10;
+    this.scene.add(this.engineSprite);
 };
 
 RailApp.prototype.createGUI = function() {
