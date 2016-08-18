3da4c659e37faec268322025c26876ce70450324
Tony Glover <tony.glover@nottingham.ac.uk>
2016-08-19 00:07:55 +0100

    Animated sprites around spline, added ground.

--- js/railViz.js
+++ js/railViz.js
@@ -39,10 +39,8 @@ RailApp.prototype.init = function(container) {
     ];
 
     this.tripTime = this.data[this.data.length-1].time;
+    this.tripTime *= 2;
     this.animating = false;
-
-    this.binormal = new THREE.Vector3();
-    this.normal = new THREE.Vector3();
 };
 
 RailApp.prototype.update = function() {
@@ -53,6 +51,12 @@ RailApp.prototype.update = function() {
         this.realTime += (delta * this.realTimeInc);
         this.delayTime += (delta * this.delayTimeInc);
         $('#minutes').html(this.realTime < 10 ? '0' + Math.floor(this.realTime) : Math.floor(this.realTime));
+        var pos = this.tube.parameters.path.getPointAt( this.currentTime / this.tripTime );
+        this.engineSprite.position.set(pos.x, pos.y+this.trainHeight, pos.z+this.trackOffset);
+
+        pos = this.tube.parameters.path.getPointAt( this.delayTime / this.tripTime );
+        this.ghostSprite.position.set(pos.x, pos.y+this.trainHeight, pos.z+this.trackOffset);
+
         //DEBUG
         //console.log("Real time = ", this.realTime);
 
@@ -76,43 +80,7 @@ RailApp.prototype.update = function() {
             var delay = this.data[this.currentStop+1].delay - this.data[this.currentStop].delay + this.interStopTime;
             this.delayTimeInc = delay/this.interStopTime;
         }
-        var angle = (this.currentTime/this.roundTripTime) * 2 * Math.PI;
-        var xPos = Math.sin(angle)*this.trainRadius;
-        var zPos = Math.cos(angle)*this.trainRadius;
-        this.engineSprite.position.set(xPos, 5, zPos);
-        angle = (this.delayTime/this.roundTripTime) * 2 * Math.PI;
-        xPos = Math.sin(angle)*this.trainRadius;
-        zPos = Math.cos(angle)*this.trainRadius;
-        this.ghostSprite.position.set(xPos, 5, zPos);
     }
-
-    //Move along spline
-    var time = Date.now();
-    var looptime = 20 * 1000;
-    var t = ( time % looptime ) / looptime;
-
-    var pos = this.tube.parameters.path.getPointAt( t );
-    //pos.multiplyScalar( scale );
-
-    // interpolation
-    var segments = this.tube.tangents.length;
-    var pickt = t * segments;
-    var pick = Math.floor( pickt );
-    var pickNext = ( pick + 1 ) % segments;
-
-    this.binormal.subVectors( this.tube.binormals[ pickNext ], this.tube.binormals[ pick ] );
-    this.binormal.multiplyScalar( pickt - pick ).add( this.tube.binormals[ pick ] );
-
-
-    var dir = this.tube.parameters.path.getTangentAt( t );
-
-    var offset = 1;
-
-    this.normal.copy( this.binormal ).cross( dir );
-
-    // We move on a offset on its binormal
-    pos.add( this.normal.clone().multiplyScalar( offset ) );
-    this.sphere.position.set(pos.x, pos.y+5, pos.z-100);
 
     BaseApp.prototype.update.call(this);
 };
@@ -120,90 +88,12 @@ RailApp.prototype.update = function() {
 RailApp.prototype.createScene = function() {
     BaseApp.prototype.createScene.call(this);
 
-    var halfWidth = 100, depth = 100;
-    var points = [];
-    points.push(new THREE.Vector3(0, 0, 0));
-    points.push(new THREE.Vector3(halfWidth, 0, 0));
-    points.push(new THREE.Vector3(halfWidth, 0, -depth));
-    points.push(new THREE.Vector3(-halfWidth, 0, -depth));
-    points.push(new THREE.Vector3(-halfWidth, 0, 0));
-    points.push(new THREE.Vector3(0, 0, 0));
-
-    this.railGroup = new THREE.Object3D();
-    this.railGroup.name = "railway";
-
-    var innerRad = 225, outerRad = 240, segments = 30;
-    var ringGeom = new THREE.RingGeometry(innerRad, outerRad, segments);
-    var ringMat = new THREE.MeshBasicMaterial( {color: 0x373737} );
-    var ringMesh = new THREE.Mesh(ringGeom,  ringMat);
-    //var yScaleFactor = 0.55;
-    //ringMesh.scale.y = yScaleFactor;
-    ringMesh.rotation.x = -Math.PI/2;
-    //this.railGroup.add(ringMesh);
-
-    //Scene parameters
-    this.engineX = 230;
-    this.engineZ = 124.5;
-    this.a2 = this.engineX * this.engineX;
-    this.b2 = this.engineZ * this.engineZ;
-    this.sector = 1;
-
-    var textureLoader = new THREE.TextureLoader();
-    var trainTex = textureLoader.load("images/engine.png");
-    var pointTex = textureLoader.load("images/pin.png");
-
-    var trainMat = new THREE.SpriteMaterial( {map: trainTex} );
-    this.engineSprite = new THREE.Sprite(trainMat);
-    this.railGroup.add(this.engineSprite);
-    this.engineSprite.scale.set(10, 10, 1);
-    this.engineSprite.position.set(0, 5, outerRad - 7.5);
-
-    var ghostMat = new THREE.SpriteMaterial( {map: trainTex, opacity: 0.5});
-    this.ghostSprite = new THREE.Sprite(ghostMat);
-    this.railGroup.add(this.ghostSprite);
-    this.ghostSprite.scale.set(10, 10, 1);
-    this.ghostSprite.position.set(0, 5, this.trainRadius - 7.5);
-
-    var pointMat = new THREE.SpriteMaterial( {map: pointTex} );
-    var numPointers = this.data.length;
-    var pointerAngle = 0, angleInc = (2 * Math.PI) / numPointers, pointerRadius = 220;
-    var circumference = 2 * Math.PI * pointerRadius;
-
-    this.pointerSprites = [];
-    var labelPos = new THREE.Vector3(), labelScale = new THREE.Vector3(30, 30, 1);
-    var label;
-    for(var i=0; i<numPointers; ++i, pointerAngle += angleInc) {
-        this.pointerSprites.push(new THREE.Sprite(pointMat));
-        this.railGroup.add(this.pointerSprites[i]);
-        this.pointerSprites[i].scale.set(30, 30, 1);
-        this.pointerSprites[i].position.set(Math.sin(pointerAngle)*pointerRadius, 15, Math.cos(pointerAngle)*pointerRadius);
-        labelPos.set(this.pointerSprites[i].position.x, 25, this.pointerSprites[i].position.z);
-        label = spriteManager.create(this.data[i].stationName, labelPos, labelScale, 32, 1, true, false);
-        this.railGroup.add(label);
-    }
-
-    this.trainRadius = 230;
-    this.roundTripTime = this.tripTime * 2;
-    this.interStopDistance = circumference / numPointers;
-    this.movementPerSecond = circumference / this.roundTripTime;
-    this.interStopTime = this.interStopDistance / this.movementPerSecond;
-    this.currentTime = 0;
-    this.currentStop = 0;
-    this.realTime = 0;
-    this.timeToNextStop = this.interStopTime;
-    this.realTimeToNextStop = this.data[this.currentStop+1].time;
-    this.realTimeInc = this.realTimeToNextStop / this.interStopTime;
-    $('#delay').html(this.data[this.currentStop].delay);
-    //Ghost engine
-    var delay = this.data[this.currentStop+1].delay - this.data[this.currentStop].delay + this.interStopTime;
-    this.delayTime = this.data[this.currentStop].delay;
-    this.delayTimeInc = delay/this.interStopTime;
-    var angle = (this.data[this.currentStop].delay/this.roundTripTime) * 2 * Math.PI;
-    var xPos = Math.sin(angle)*this.trainRadius;
-    var zPos = Math.cos(angle)*this.trainRadius;
-    this.ghostSprite.position.set(xPos, 5, zPos);
-
-    this.scene.add(this.railGroup);
+    //Ground plane
+    var planeGeom = new THREE.PlaneBufferGeometry(1000, 1000, 8, 8);
+    var planeMat = new THREE.MeshLambertMaterial( {color: 0x1D701D});
+    var plane = new THREE.Mesh(planeGeom, planeMat);
+    plane.rotation.x = -Math.PI/2;
+    this.scene.add(plane);
 
     //Spline
     var width = 200, depth = 275;
@@ -228,15 +118,65 @@ RailApp.prototype.createScene = function() {
     trackGroup.position.z = -100;
     this.scene.add(trackGroup);
 
-    var pos = tube.parameters.path.getPointAt( 1 );
-    console.log("Point = ", pos);
-    var sphereGeom = new THREE.SphereBufferGeometry(3, 16, 16);
-    var sphereMat = new THREE.MeshLambertMaterial( {color:0x00ff00});
-    var sphere = new THREE.Mesh(sphereGeom, sphereMat);
-    this.scene.add(sphere);
-    sphere.position.set(pos.x, pos.y, pos.z);
+    var pos = tube.parameters.path.getPointAt( 0 );
+    this.trackOffset = -100;
+    this.trainHeight = 7;
+    this.pinHeight = 20;
     this.tube = tube;
-    this.sphere = sphere;
+
+    this.railGroup = new THREE.Object3D();
+    this.railGroup.name = "railway";
+
+    var textureLoader = new THREE.TextureLoader();
+    var trainTex = textureLoader.load("images/engine.png");
+    var pointTex = textureLoader.load("images/pin.png");
+
+    var trainMat = new THREE.SpriteMaterial( {map: trainTex} );
+    this.engineSprite = new THREE.Sprite(trainMat);
+    this.railGroup.add(this.engineSprite);
+    this.engineSprite.scale.set(10, 10, 1);
+    this.engineSprite.position.set(pos.x, pos.y+this.trainHeight, pos.z+this.trackOffset);
+
+    var ghostMat = new THREE.SpriteMaterial( {map: trainTex, opacity: 0.5});
+    this.ghostSprite = new THREE.Sprite(ghostMat);
+    this.railGroup.add(this.ghostSprite);
+    this.ghostSprite.scale.set(10, 10, 1);
+    this.ghostSprite.position.set(pos.x, pos.y+this.trainHeight, pos.z+this.trackOffset);
+
+    var pointMat = new THREE.SpriteMaterial( {map: pointTex} );
+    var numPointers = this.data.length;
+
+    this.pointerSprites = [];
+    var labelPos = new THREE.Vector3(), labelScale = new THREE.Vector3(30, 30, 1);
+    var label;
+    for(var i=0; i<numPointers; ++i) {
+        this.pointerSprites.push(new THREE.Sprite(pointMat));
+        this.railGroup.add(this.pointerSprites[i]);
+        this.pointerSprites[i].scale.set(30, 30, 1);
+        pos = tube.parameters.path.getPointAt( i/numPointers );
+        this.pointerSprites[i].position.set(pos.x, pos.y+this.pinHeight, pos.z+this.trackOffset);
+        labelPos.set(this.pointerSprites[i].position.x, 30, this.pointerSprites[i].position.z);
+        label = spriteManager.create(this.data[i].stationName, labelPos, labelScale, 32, 1, true, false);
+        this.railGroup.add(label);
+    }
+
+    var trackLength = tube.parameters.path.getLength();
+    this.interStopDistance = trackLength / numPointers;
+    this.movementPerSecond = trackLength / this.tripTime;
+    this.interStopTime = this.interStopDistance / this.movementPerSecond;
+    this.currentTime = 0;
+    this.currentStop = 0;
+    this.realTime = 0;
+    this.timeToNextStop = this.interStopTime;
+    this.realTimeToNextStop = this.data[this.currentStop+1].time;
+    this.realTimeInc = this.realTimeToNextStop / this.interStopTime;
+    $('#delay').html(this.data[this.currentStop].delay);
+    //Ghost engine
+    var delay = this.data[this.currentStop+1].delay - this.data[this.currentStop].delay + this.interStopTime;
+    this.delayTime = this.data[this.currentStop].delay;
+    this.delayTimeInc = delay/this.interStopTime;
+
+    this.scene.add(this.railGroup);
 };
 
 RailApp.prototype.startStopAnimation = function() {
